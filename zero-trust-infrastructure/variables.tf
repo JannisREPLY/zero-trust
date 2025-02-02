0ee58e8affa40bcec5f054b1549ad03037f2ed9a
Jannis Schoormann <jannis.schoormann@external.biontech.de>
2025-02-02 14:14:29 +0100

    add variables

--- zero-trust-infrastructure/variables.tf
+++ zero-trust-infrastructure/variables.tf
@@ -4,6 +4,12 @@ variable "aws_region" {
   default     = "eu-central-1"
 }
 
+variable "aws_region_replication" {
+  description = "AWS region to deploy replication resources"
+  type = string
+  default = "eu-west-1"
+}
+
 variable "allowed_ip" {
   description = "IP that is allowed to access the web application"
   type        = string
